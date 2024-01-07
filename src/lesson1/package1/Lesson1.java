7f1c1df0da228877753d7b36c81b4f52faec24fd
Siarhei <greysiarhei@gmail.com>
2024-01-07 14:05:41 +0300

    add new line

--- src/lesson1/package1/Lesson1.java
+++ src/lesson1/package1/Lesson1.java
@@ -1,4 +1,4 @@
-package lesson1;
+package lesson1.package1;
 
 public class Lesson1 {
     public static void main(String[] args) {
---
+++ src/lesson1/package2/Lesson1Package1.java
@@ -0,0 +1,29 @@
+package lesson1.package2;
+
+public class Lesson1Package1 {
+
+    // Line Comment
+    /* Block comment
+     */
+
+    //psvm
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Hello again AQA-7");
+
+        //format CTRL+ALT+L
+        System.out.println("Hello again AQA-7");
+        System.out.println("Hello again AQA-7");
+        System.out.println("Hello again AQA-7");
+        System.out.println("Hello again AQA-7");
+        System.out.println("Hello again AQA-7");
+        System.out.println("Hello again AQA-7");
+        System.out.println("Hello again AQA-7");
+
+        int myFirstVariableInTheWorld = 9;
+        int variableWithoutInit, secondVariableWithoutInit;
+        variableWithoutInit = 3;
+        System.out.println(variableWithoutInit);
+    }
+
+}
